6d1ca1dbe7b6c76d29cbcef515f43c58ccae98f1
searly <searly5@yahoo.com>
2021-02-10 13:24:36 -0600

    completed

--- queries.sql
+++ queries.sql
@@ -1,7 +1,9 @@
 ## Part 1: Test it with SQL
-
+SHOW COLUMNS FROM job;
 ## Part 2: Test it with SQL
+SELECT * from employer WHERE location="St. Louis City";
 
 ## Part 3: Test it with SQL
+DROP TABLE job;
 
 ## Part 4: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,27 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+   @Size(max = 500)
+    private String description;
+
+   @ManyToMany(mappedBy="skills")
+    private List<Job> jobs = new ArrayList<>();
+
+   public Skill() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
